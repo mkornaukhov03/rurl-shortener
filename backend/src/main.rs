431bd873f1acf873150b176d52ec1ddee13c889e
Mikhail Kornaukhov <66915223+mkornaukhov03@users.noreply.github.com>
2025-05-02 19:43:48 +0300

    Add redis and some improvements in backend (#2)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -8,11 +8,132 @@ use axum::{
     routing::{get, post},
 };
 use axum_macros::debug_handler;
-use tokio::sync::RwLock;
+use tokio::sync::{Mutex, RwLock};
 
-#[derive(Default)]
+// TODO use pool of connections instead of one
+struct RedisSingleConnection {
+    conn: Mutex<redis::aio::MultiplexedConnection>,
+}
+
+impl RedisSingleConnection {
+    async fn new(endpoint: String) -> Self {
+        let client =
+            redis::Client::open(format!("redis://{}/", endpoint)).expect("Cannot connect to redis");
+
+        RedisSingleConnection {
+            conn: Mutex::new(
+                client
+                    .get_multiplexed_async_connection()
+                    .await
+                    .expect("Cannot get async connection"),
+            ),
+        }
+    }
+
+    async fn store(&self, short: String, url: String) -> bool {
+        // Set key=short with value=url if not set yet atomically
+        // TODO think about for how much time to store?
+        match self
+            .conn
+            .lock()
+            .await
+            .send_packed_command(
+                redis::cmd("SET")
+                    .arg(short)
+                    .arg(url)
+                    .arg("EX")
+                    .arg(3600)
+                    .arg("NX"),
+            )
+            .await
+        {
+            Ok(resp) => match resp {
+                redis::Value::Okay => return true,
+                redis::Value::Nil => return false,
+                _ => {
+                    log::warn!("Response from redis store is not OK, nor nil");
+                    return false;
+                }
+            },
+            Err(e) => {
+                log::error!("Error to store in redis: {}", e);
+                return false;
+            }
+        }
+    }
+
+    async fn fetch(&self, short: &str) -> Option<String> {
+        match self
+            .conn
+            .lock()
+            .await
+            .send_packed_command(redis::cmd("GET").arg(short))
+            .await
+        {
+            Ok(resp) => match resp {
+                redis::Value::SimpleString(s) => {
+                    return Some(s);
+                }
+                redis::Value::Nil => {
+                    return None;
+                }
+                redis::Value::BulkString(s) => match String::from_utf8(s) {
+                    Ok(s) => {
+                        return Some(s);
+                    }
+                    Err(_) => {
+                        log::warn!("Non utf-8 url is stored");
+                        return None;
+                    }
+                },
+                _ => {
+                    log::warn!("Stored value is not string");
+                    return None;
+                }
+            },
+            Err(e) => {
+                log::error!("Error to fetch from redis: {}", e);
+                return None;
+            }
+        }
+    }
+}
+
+// TODO validation of url
+#[allow(unused)]
+enum Storage {
+    NonPersistent(RwLock<HashMap<String, String>>),
+    Redis(RedisSingleConnection),
+}
+
+impl Storage {
+    async fn store(&self, short: String, url: String) -> bool {
+        match self {
+            Storage::NonPersistent(rw_lock) => {
+                let mut guard = rw_lock.write().await;
+                if guard.contains_key(&short) {
+                    false
+                } else {
+                    guard.insert(short, url);
+                    true
+                }
+            }
+            Storage::Redis(redis_single_connection) => {
+                redis_single_connection.store(short, url).await
+            }
+        }
+    }
+
+    async fn fetch(&self, short: &str) -> Option<String> {
+        match self {
+            Storage::NonPersistent(rw_lock) => rw_lock.read().await.get(short).cloned(),
+            Storage::Redis(redis_single_connection) => redis_single_connection.fetch(short).await,
+        }
+    }
+}
+
 struct AppState {
-    link_map: RwLock<HashMap<String, String>>,
+    storage: Storage,
     config: Config,
 }
 
@@ -20,13 +141,8 @@ struct AppState {
 struct Config {
     port: u16,
     host: String,
+    redis_endpoint: String,
 }
-
-/*
-    GET  /%SHORT%
-    GET  /status
-    POST /
-*/
 
 fn config_from_env() -> Config {
     let port: u16 = env::var("RURL_PORT")
@@ -36,17 +152,27 @@ fn config_from_env() -> Config {
 
     let host = env::var("RURL_HOST").expect("Provide host via RURL_HOST");
 
+    // TODO fallback into in memory or fail?
+    let redis_endpoint = env::var("RURL_REDIS_ENDPOINT").unwrap_or("redis:6379".into());
+
     Config {
         port,
         host,
+        redis_endpoint,
     }
 }
 
 #[tokio::main]
 async fn main() {
+    // Env logger is for development purposes
+    env_logger::init();
+
+    let config = config_from_env();
+
+    let storage = Storage::Redis(RedisSingleConnection::new(config.redis_endpoint).await);
     let state = Arc::new(AppState {
         config: config_from_env(),
-        ..Default::default()
+        storage: storage,
     });
 
     let app = Router::new()
@@ -55,31 +181,34 @@ async fn main() {
         .route("/", post(handle_post))
         .with_state(state.clone());
 
-    let listener =
-        tokio::net::TcpListener::bind(format!("{}:{}", (*state).config.host, state.config.port))
-            .await
-            .unwrap();
+    let addr = format!("{}:{}", (*state).config.host, state.config.port);
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    log::info!("Starting to accept clients on {addr}");
     axum::serve(listener, app).await.unwrap();
 }
 
 #[debug_handler]
 async fn handle_status(State(_state): State<Arc<AppState>>) -> StatusCode {
+    log::info!("GET /status");
     http::StatusCode::OK
 }
 
 #[debug_handler]
 async fn handle_post(
     State(state): State<Arc<AppState>>,
-    Json(params): Json<HashMap<String, String>>,
+    Json(mut params): Json<HashMap<String, String>>,
 ) -> StatusCode {
-    match (params.get("old"), params.get("short")) {
+    log::info!("POST / ({:?})", params);
+    match (params.remove("old"), params.remove("short")) {
         (Some(old), Some(short)) => {
-            let mut link_map = state.link_map.write().await;
-            if link_map.contains_key(short) || short == "status" {
+            if short == "status" {
                 return http::StatusCode::CONFLICT;
             }
-            link_map.insert(short.clone(), old.clone());
-            http::StatusCode::OK
+            if state.storage.store(short, old).await {
+                http::StatusCode::OK
+            } else {
+                http::StatusCode::CONFLICT
+            }
         }
         _ => http::StatusCode::BAD_REQUEST,
     }
@@ -87,7 +216,8 @@ async fn handle_post(
 
 #[debug_handler]
 async fn handle_get(State(state): State<Arc<AppState>>, Path(path): Path<String>) -> Response {
-    match state.link_map.read().await.get(&path) {
+    log::info!("GET /{}", path);
+    match state.storage.fetch(&path).await {
         Some(full) => {
             return (
                 StatusCode::MOVED_PERMANENTLY,
