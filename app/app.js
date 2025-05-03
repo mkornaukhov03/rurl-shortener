4d5af24d6939cfa7513ef95b7a604d3bfd9aef18
Vyacheslav Shabes <vyacheslav.shabes@yandex.ru>
2025-05-04 00:11:30 +0300

    add url validation

--- app/app.js
+++ app/app.js
@@ -3,6 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
     shortenButton.addEventListener('click', shortenLink);
     function shortenLink() {
         const inputLink = document.getElementById('inputLink').value;
+        if (!validateURL(inputLink)) {
+            console.error('Error: input link has wrong format');
+            document.getElementById('shortenedLink').textContent = "Input link has wrong format";
+            return;
+        }
         const data = {
             url: inputLink,
         };
@@ -17,7 +22,8 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(response => response.json())
         .then(data => {
             console.log('Server response:', data);
-            document.getElementById('shortenedLink').textContent = `${window.appConfig.NGINX_URL}/${data.short}`;
+            const shortenedLink = `${window.appConfig.NGINX_URL}/${data.short}`;
+            document.getElementById('shortenedLink').textContent = shortenedLink;
         })
         .catch(error => {
             console.error('Error:', error);
@@ -25,3 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+function validateURL(url) {
+    const regex = /^(?:http|https):\/\/(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::\d+)?(?:\/?|[\/?]\S+)$/i;    
+    return regex.test(url);
+}        
