18831bcc15a8fae9bd9d5bc4820c637caa5deb64
Vyacheslav Shabes <vyacheslav.shabes@yandex.ru>
2025-05-04 00:47:59 +0300

    structured files

--- app/app.js
+++ app/app.js
@@ -1,3 +1,5 @@
+import { validateURL } from './utils.js';
+
 document.addEventListener('DOMContentLoaded', function() {
     const shortenButton = document.getElementById('shortenButton');
     shortenButton.addEventListener('click', shortenLink);
@@ -31,7 +33,3 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
-function validateURL(url) {
-    const regex = /^(?:http|https):\/\/(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::\d+)?(?:\/?|[\/?]\S+)$/i;    
-    return regex.test(url);
-}        
---
+++ app/utils.js
@@ -0,0 +1,6 @@
+function validateURL(url) {
+    const regex = /^(?:http|https):\/\/(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::\d+)?(?:\/?|[\/?]\S+)$/i;    
+    return regex.test(url);
+}
+
+export { validateURL };
